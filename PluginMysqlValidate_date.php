65e4a5297f5e5a6837af40d0ff99602453ec3c86
Claes Östlund <claes@stenbergit.se>
2023-08-17 17:37:53 +0200

    1.3.0

--- PluginMysqlValidate_date.php
+++ PluginMysqlValidate_date.php
@@ -9,7 +9,7 @@ class PluginMysqlValidate_date{
   public function validate_less_or_equal($field, $form, $data = array()){
     wfPlugin::includeonce('wf/array');
     $data = new PluginWfArray($data);
-    if(wfArray::get($form, "items/$field/is_valid") && strlen(wfArray::get($form, "items/$field/post_value"))){
+    if(wfArray::get($form, "items/$field/is_valid") && wfPhpfunc::strlen(wfArray::get($form, "items/$field/post_value"))){
       if(strtotime(wfArray::get($form, "items/$field/post_value")) > strtotime(wfArray::get($form, "items/".$data->get('field')."/post_value"))){
         $form = wfArray::set($form, "items/$field/is_valid", false);
         $form = wfArray::set($form, "items/$field/errors/", $this->i18n->translateFromTheme('?label_1 is after ?label_2', array('?label_1' => wfArray::get($form, "items/$field/label"), '?label_2' => wfArray::get($form, "items/".$data->get('field')."/label"))));
