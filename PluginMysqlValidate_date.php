3ef8d3cf8168ff08144d8af81458f086dd6be15c
costlund <claes@webforms.se>
2024-05-15 11:23:07 +0200

    1.3.2

--- PluginMysqlValidate_date.php
+++ PluginMysqlValidate_date.php
@@ -10,7 +10,7 @@ class PluginMysqlValidate_date{
     wfPlugin::includeonce('wf/array');
     $data = new PluginWfArray($data);
     if(wfArray::get($form, "items/$field/is_valid") && wfPhpfunc::strlen(wfArray::get($form, "items/$field/post_value"))){
-      if(strtotime(wfArray::get($form, "items/$field/post_value")) > strtotime(wfArray::get($form, "items/".$data->get('field')."/post_value"))){
+      if(strtotime((string)wfArray::get($form, "items/$field/post_value")) > strtotime((string)wfArray::get($form, "items/".$data->get('field')."/post_value"))){
         $form = wfArray::set($form, "items/$field/is_valid", false);
         $form = wfArray::set($form, "items/$field/errors/", $this->i18n->translateFromTheme('?label_1 is after ?label_2', array('?label_1' => wfArray::get($form, "items/$field/label"), '?label_2' => wfArray::get($form, "items/".$data->get('field')."/label"))));
       }
@@ -20,7 +20,7 @@ class PluginMysqlValidate_date{
   public function validate_first_day_in_month($field, $form, $data = array()){
     $form = new PluginWfArray($form);
     if($form->get("items/$field/is_valid")){
-      if( date('d', strtotime($form->get("items/$field/post_value"))) !='01'){
+      if( date('d', strtotime((string)$form->get("items/$field/post_value"))) !='01'){
         $form->set("items/$field/is_valid", false);
         $form->set("items/$field/errors/", $this->i18n->translateFromTheme('?label_1 must be first day in a month.', array('?label_1' => $form->get("items/$field/label"))));
       }
@@ -30,7 +30,7 @@ class PluginMysqlValidate_date{
   public function validate_last_day_in_month($field, $form, $data = array()){
     $form = new PluginWfArray($form);
     if($form->get("items/$field/is_valid") && $form->get("items/$field/post_value")){
-      if( date('Y-m-t', strtotime($form->get("items/$field/post_value"))) != $form->get("items/$field/post_value")){
+      if( date('Y-m-t', strtotime((string)$form->get("items/$field/post_value"))) != $form->get("items/$field/post_value")){
         $form->set("items/$field/is_valid", false);
         $form->set("items/$field/errors/", $this->i18n->translateFromTheme('?label_1 must be last day in a month.', array('?label_1' => $form->get("items/$field/label"))));
       }
